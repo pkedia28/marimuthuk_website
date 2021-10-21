b823ee98ad9c2d9a47215eba3dc0c87eeb802709
geek484 <56823984+geek484@users.noreply.github.com>
2021-10-21 09:47:05 +0530

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -21,28 +21,3 @@ else{
 		position:'static'
 	});
 }
-
-// $(document).ready(function() {
-// 	if(screen.width>769){
-// $('.sidebar-nav').addClass('fixme');
-// }
-// });
-// var fixmeTop = $('.fixme').offset().top;       // get initial position of the element
-// $(window).scroll(function() {                  // assign scroll event listener
-
-//     var currentScroll = $(window).scrollTop(); // get current position
-//    if ($(window).width() > 969) {
-//     if (currentScroll >= fixmeTop) {           // apply position: fixed if you
-//         $('.fixme').css({                      // scroll to that element or below it
-//             position: 'fixed',
-//             top: '0',
-//         });
-//     } 
-//     else {                                   // apply position: static
-//         $('.fixme').css({                      // if you scroll above it
-//             position: 'static'
-//         });
-//     }
-// }
-
-// });
